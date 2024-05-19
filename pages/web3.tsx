947c93ee15f557c11fb832bd432294d1f9cee535
Joey <2390923149@qq.com>
2024-05-19 11:26:37 +0800

    feat: 钱包基本交互

--- pages/web3.tsx
+++ pages/web3.tsx
@@ -1,29 +1,52 @@
 import { createConfig, http, useReadContract, useWriteContract, useWatchContractEvent } from "wagmi";
-import { mainnet } from "wagmi/chains";
-import { WagmiWeb3ConfigProvider, MetaMask } from "@ant-design/web3-wagmi";
-import { Address, NFTCard, Connector, ConnectButton, useAccount } from "@ant-design/web3";
-import { injected } from "wagmi/connectors";
+import { goerli, mainnet, sepolia, polygon } from "wagmi/chains";
+import { WagmiWeb3ConfigProvider, MetaMask, Goerli, Sepolia, WalletConnect, Polygon } from "@ant-design/web3-wagmi";
+import { Address, NFTCard, Connector, ConnectButton, useAccount, useProvider } from "@ant-design/web3";
+import { injected, walletConnect } from "wagmi/connectors";
 import { Button, message } from 'antd'
 import { parseEther } from 'viem'
 
 
 const config = createConfig({
-  chains: [mainnet],
+  chains: [mainnet, polygon],
   transports: {
-    [mainnet.id]: http()
+    [mainnet.id]: http(),
+    [polygon.id]: http(),
+    // [sepolia.id]: http(),
     // [mainnet.id]: http("https://eth-mainnet.g.alchemy.com/v2/4fjyGSbuZ7RgldDMpvSKmlv9cUAziQFm")
   },
   connectors: [
     injected({
       target: 'metaMask' as any
+    }),
+    walletConnect({
+      // 去官方申请自己的应用ID
+      //  https://cloud.walletconnect.com/
+      projectId: 'c07c0051c2055890eade3556618e38a6',
+      showQrModal: true
     })
   ]
 
 })
 
+const contractInfo = [
+  {
+    id: 1,
+    name: "Ethereum",
+    contractAddress: "0xEcd0D12E21805803f70de03B72B1C162dB0898d9",
+  },
+  {
+    id: 137,
+    name: "Polygon",
+    contractAddress: "0x418325c3979b7f8a17678ec2463a74355bdbe72c",
+  },
+]
+
 const CallTest = () => {
   const { account } = useAccount()
   console.log("account: ", account)
+  const { chain } = useProvider()
+
   const result = useReadContract({
     abi: [
       {
@@ -34,11 +57,13 @@ const CallTest = () => {
         outputs: [{ type: 'uint256' }],
       },
     ],
-    address: '0xEcd0D12E21805803f70de03B72B1C162dB0898d9',
+    // address: '0xc2b965B02ee38157E99c88c917C5F816D9d97292',
+    address: contractInfo.find(e => e.id === chain?.id)?.contractAddress as `0x${string}`,
     functionName: 'balanceOf',
     args: [account?.address as `0x${string}`],
     // args: [`0xEcd0D12E21805803f70de03B72B1C162dB0898d9` as `0x${string}`],
   })
+  console.log('result: ', result)
 
   const { writeContract } = useWriteContract()
 
@@ -59,7 +84,9 @@ const CallTest = () => {
           outputs: [],
         },
       ],
-      address: "0xEcd0D12E21805803f70de03B72B1C162dB0898d9",
+      // address: "0xEcd0D12E21805803f70de03B72B1C162dB0898d9",
+      // address: "0xc2b965B02ee38157E99c88c917C5F816D9d97292",
+      address: contractInfo.find(e => e.id === chain?.id)?.contractAddress as `0x${string}`,
       functionName: "mint",
       args: [1],
       value: parseEther("0.01"),
@@ -74,34 +101,34 @@ const CallTest = () => {
     })
   }
 
-  useWatchContractEvent({
-    address: '0xEcd0D12E21805803f70de03B72B1C162dB0898d9',
-    abi: [
-      {
-        anonymous: false,
-        inputs: [
-          {
-            indexed: false,
-            internalType: "address",
-            name: "minter",
-            type: "address",
-          },
-          {
-            indexed: false,
-            internalType: "uint256",
-            name: "amount",
-            type: "uint256",
-          },
-        ],
-        name: "Minted",
-        type: "event",
-      },
-    ],
-    eventName: "Minted",
-    onLogs() {
-      message.success("new minted!");
-    },
-  })
+  // useWatchContractEvent({
+  //   address: '0xEcd0D12E21805803f70de03B72B1C162dB0898d9',
+  //   abi: [
+  //     {
+  //       anonymous: false,
+  //       inputs: [
+  //         {
+  //           indexed: false,
+  //           internalType: "address",
+  //           name: "minter",
+  //           type: "address",
+  //         },
+  //         {
+  //           indexed: false,
+  //           internalType: "uint256",
+  //           name: "amount",
+  //           type: "uint256",
+  //         },
+  //       ],
+  //       name: "Minted",
+  //       type: "event",
+  //     },
+  //   ],
+  //   eventName: "Minted",
+  //   onLogs() {
+  //     message.success("new minted!");
+  //   },
+  // })
 
   return (
     <div>
@@ -115,7 +142,7 @@ const CallTest = () => {
 
 
 export default function Web3() {
-  return <WagmiWeb3ConfigProvider config={ config } wallets={ [MetaMask()] }>
+  return <WagmiWeb3ConfigProvider config={ config } wallets={ [MetaMask(), WalletConnect()] } chains={[Polygon]}>
     <Address format address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9" />
     <NFTCard
       address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9"

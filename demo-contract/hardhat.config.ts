c7a656adc08ec29566be8250269e22d9e49a3f35
Joey <2390923149@qq.com>
2024-06-02 09:00:53 +0800

    feat: hardhat尝试部署speolia链

--- demo-contract/hardhat.config.ts
+++ demo-contract/hardhat.config.ts
@@ -3,6 +3,12 @@ import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    speolia: {
+      url: "https://sepolia.infura.io/v3/privateKey",
+      accounts: ['privateKey']
+    }
+  }
 };
 
 export default config;
